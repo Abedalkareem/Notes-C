38e31f3c7c29ecb45f91f778916f4c895fed5611
abedalkareem omreyh <abedalkareem.omreyh@yahoo.com>
2022-03-09 06:40:48 +0400

    fix note input

--- notes/outputer/outputer.c
+++ notes/outputer/outputer.c
@@ -10,15 +10,18 @@
 #include "note.h"
 #include <stdlib.h>
 
+#define MAX_NOTE_TITLE 50
+#define MAX_NOTE 200
+
 void startChoicesSelection(void) {
   printf("Hi! What do you want to do?\n");
   printf("1-Get all the notes\n");
   printf("2-Insert a new note\n");
   printf("3-Delete a note\n");
 
-  int choice;
-  scanf("%i", &choice);
-  switch (choice) {
+  char *choice = malloc(sizeof(char) * 2);
+  gets(choice);
+  switch (atoi(choice)) {
     case 1:
       printAllNotes();
       break;
@@ -29,6 +32,7 @@ void startChoicesSelection(void) {
       startNoteDeletion();
       break;
   }
+  free(choice);
 }
 
 void printAllNotes(void) {
@@ -42,7 +46,7 @@ void printAllNotes(void) {
     printf("No notes found.\n\n");
     return;
   }
-
+  
   printf("Notes List:\n");
   for (int i = 0; i < size; i++) {
     struct Note note = notes[i];
@@ -56,12 +60,12 @@ void printAllNotes(void) {
 }
 
 void startNoteInsertion(void) {
-  char title[50];
-  char note[100];
+  char title[MAX_NOTE_TITLE];
+  char note[MAX_NOTE];
   printf("Please enter the title of the note\n");
-  scanf("%s", title);
+  gets(title);
   printf("Please enter the note\n");
-  scanf("%s", note);
+  gets(note);
   addNote(title, note);
   printf("\n");
 }
@@ -70,8 +74,9 @@ void startNoteDeletion(void)  {
   printAllNotes();
   printf("\n");
   printf("Which note do you want to delete, please enter the id.\n");
-  int choice;
-  scanf("%i", &choice);
-  deleteNote(choice);
+  char *choice = malloc(sizeof(char) * 2);
+  gets(choice);
+  deleteNote(atoi(choice));
   printf("Note deleted successfully!.\n");
+  free(choice);
 }
